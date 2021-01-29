e0aea373d8dfc0aedb42990ab73d0002f8a9bcd4
Daren McCarrellgit config --global user.email darenmcc16@gmail.com <darenmcc16@gmail.com>
2021-01-29 15:40:24 -0600

    deploy steps taken

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ const MOVIES = require('./movies-data-small.json')
 
 const app = express()
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 app.use(cors())
 app.use(helmet())
 
@@ -41,7 +42,18 @@ app.get('/movie', function handleGetMovie(req, res){
     }
 })
 
-const PORT = 8000
+app.use((error, req, res, next) =>{
+  let response
+  if(process.env.NODE_ENV === 'production'){
+    response = {error: {message: 'server error'}}
+  }
+  else{
+    response = {error}
+    }
+    res.status(500).json(response)
+})
+
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () =>{
   console.log('server is listening')
